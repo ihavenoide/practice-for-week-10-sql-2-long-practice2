0e0bcb9e38a58d27b1cb5cd0740898703315fcca
James <james.hernandez1225@gmail.com>
2023-04-27 19:24:47 -0400

    all done

--- sql-practice/phase-5/query-5.sql
+++ sql-practice/phase-5/query-5.sql
@@ -1,7 +1,14 @@
 -- Find names of the cats whose owners are both George Beatty and Melynda Abshire, or just George Beatty, or just Melynda Abshire
 -- Your code here
-SELECT * FROM cats JOIN owners JOIN cat_owners
-ON(cat_owners.cat_id = cats.id AND cat_owners.owner_id = owners.id);
-SELECT cats.name FROM cats JOIN owners JOIN cat_owners
-ON(cat_owners.cat_id = cats.id AND cat_owners.owner_id = owners.id)
-WHERE owners.first_name IN('George' , 'Melynda') AND owners.last_name IN( 'Beatty','Abshire');
+-- SELECT * FROM cats JOIN owners JOIN cat_owners
+-- ON(cat_owners.cat_id = cats.id AND cat_owners.owner_id = owners.id);
+-- SELECT cats.name FROM cats JOIN owners JOIN cat_owners
+-- ON(cat_owners.cat_id = cats.id AND cat_owners.owner_id = owners.id)
+-- WHERE owners.first_name = "George" and owners.first_name = "Melynda"
+-- WHERE owners.first_name IN('George' , 'Melynda') AND owners.last_name IN( 'Beatty','Abshire')
+SELECT cats.name
+FROM cats
+JOIN cat_owners ON (cats.id = cat_owners.cat_id)
+JOIN owners ON (owners.id = cat_owners.owner_id)
+WHERE owners.first_name IN ('George', 'Melynda')
+GROUP BY cats.id;
